b894e770a4731890fe2bad328d2a7c35c513a0d5
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-31 11:02:40 +0530

    cw

---
+++ Python Classwork/CW2.py
@@ -0,0 +1,14 @@
+"""
+You work for a bookstore that generates receipts for customers. Your task is to prepare a simple receipt using string techniques.
+Here’s what you need to do:
+Create a multiline string to store the receipt header.
+The customer bought 2 items:
+Book Title: "Python Basics" – ₹450
+Book Title: "Data Science Intro" – ₹600
+For each line showing the book and price, use a single string with basic {} placeholders and call format() once to fill in the values.
+Calculate the total price and add it similarly.
+Concatenate a thank-you message at the end.
+Make sure the output uses newline (\n) and tab (\t) for readability.
+Display the entire receipt in uppercase.
+
+"""
