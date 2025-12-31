8ae3744d534b581c32414c94b3f062803740766c
Angeleena Livingston D <angeleenalivingston@gmail.com>
2025-12-31 09:24:32 +0530

    PYTHON

---
+++ HW1.py
@@ -0,0 +1,17 @@
+"""Homework Problem:
+A grocery store wants to calculate the final bill for a customer. The store has 3 products: rice, sugar, and oil. Each product has a fixed price per kilogram:
+Rice: ?45 per kg
+Sugar: ?40 per kg
+Oil: ?130 per kg
+Assume a customer bought:
+3 kg of rice
+2.5 kg of sugar
+1.8 kg of oil
+Your task:
+Use variables to store the prices and quantities.
+Use appropriate data types.
+Calculate and print the total price for each item and the final total bill.
+Show the total bill as an integer and also as a string.
+Convert the float values where needed using explicit conversion.
+Use random number generation to add a random ?5–?10 delivery charge.
+Show the final bill amount including delivery charge."""
