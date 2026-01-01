68e52367586b322b21dd13b92b4e97a89287f67d
Angeleena Livingston D <angeleenalivingston@gmail.com>
2026-01-01 10:46:45 +0530

    cw

--- Python Homework/HW2.py
+++ Python Homework/HW2.py
@@ -1,13 +1,1 @@
-""""
-Write a Python program that does the following:
-Store a short paragraph about a Python course using a multiline string.
-Display the length of the paragraph (number of characters).
-Display the first and last characters in the paragraph.
-Slice and print a short preview: the first 50 characters.
-Replace all occurrences of the word "Python" with "PYTHON" (in all caps).
-Convert the entire paragraph to lowercase.
-Remove any extra whitespaces from the start or end.
-Split the paragraph into individual words and print the list.
-Check if the word "course" exists in the paragraph. Print a message if found.
-Display the final message:
-"The course description is {} characters long and has {} words." using the format() method."""
+
