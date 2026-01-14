698c086b88284c35569dbdb14ae85ae45dc29d06
Angeleena Livingston D <angeleenalivingston@gmail.com>
2026-01-14 10:09:55 +0530

    py hw

--- Python Homework/HW11.py
+++ Python Homework/HW11.py
@@ -7,3 +7,30 @@ Convert the list of records into a JSON string and print it.
 Parse the JSON back to a Python object and display each record on a separate line.
 
 """
+
+
+from tracker import create_record
+from datetime import datetime
+import json
+
+records = [
+    create_record("Paris", "Loved the Eiffel Tower", "05-06-2022"),
+    create_record("Rome", " history place", "18-08-2023"),
+    create_record("Tokyo", "Scenary was incredible", "10-04-2024")
+]
+
+
+for record in records:
+    date_obj = datetime.strptime(record["date"], "%d-%m-%Y")
+    record["date"] = date_obj.strftime("%B %d, %Y")
+
+json_data = json.dumps(records, indent=4)
+print("JSON Output:")
+print(json_data)
+
+
+parsed_data = json.loads(json_data)
+
+print("\nParsed Records:")
+for rec in parsed_data:
+    print(f'{rec["city"]} | {rec["comment"]} | {rec["date"]}')
