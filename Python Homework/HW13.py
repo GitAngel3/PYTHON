9c7b120e8aab67f950225d996fdbe35a0e34ff13
Angeleena Livingston D <angeleenalivingston@gmail.com>
2026-01-16 10:57:42 +0530

    py

--- Python Homework/HW13.py
+++ Python Homework/HW13.py
@@ -9,3 +9,26 @@ If the file already exists, first show all existing names, then add the new ones
 
 After saving the new names, read and display the updated list of all student names."""
 
+
+
+n= int(input('Enter the count of student names you want to add:'))
+
+
+for i in range(n):
+    names = []
+    name = input(f"Enter names {i+1}: ")
+    names.append(name)
+
+print("List of names:", names)
+
+
+
+
+f = open("students.txt", "a")
+f.write(item)
+f.close()
+f=open("items.txt", "r")
+print("\nUpdated list of stationery items:")
+print(f.read())
+
+
