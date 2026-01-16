c71faec13a27659260c46e60be439d6bb0da0863
Angeleena Livingston D <angeleenalivingston@gmail.com>
2026-01-16 09:21:54 +0530

    py

--- Python Homework/HW12.py
+++ Python Homework/HW12.py
@@ -5,3 +5,21 @@ If all inputs are valid, print a thank-you message along with the user's name an
 Use the try, except, and finally blocks to structure your code safely.
 
 """
+try:
+    name = input("Enter your name: ").strip()
+    feedback = input("Enter your feedback: ").strip()
+
+   
+    if not name or not feedback:
+        raise ValueError("Name and feedback cannot be empty.")
+
+except ValueError as e:
+    print("Error:", e)
+
+else:
+    print("\nThank you for your feedback!")
+    print("Name:", name)
+    print("Feedback:", feedback)
+
+finally:
+    print("\n--- Feedback process completed ---")
